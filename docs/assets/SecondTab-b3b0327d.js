80f12612aa550025efb89874d36d7ebd9bbc5973
Mauro Perez <maurop123@gmail.com>
2023-03-14 23:41:02 -0600

    build

--- docs/assets/SecondTab-b3b0327d.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as a,a as c,_ as n}from"./index-329289c9.js";const s={__name:"SecondTab",setup(o){return(t,_)=>(e(),a("main",null,[c(n)]))}};export{s as default};
---
+++ docs/assets/SecondTab-bd46bcff.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,a as c,_ as n}from"./index-83c8b807.js";const s={__name:"SecondTab",setup(o){return(t,_)=>(e(),a("main",null,[c(n)]))}};export{s as default};
