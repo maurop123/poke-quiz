12ca437ee5d499a05bd0fb2db9119ff5b96745d0
Mauro Perez <maurop123@gmail.com>
2023-03-21 23:46:15 -0400

    fix: reloading on sub pages
    turn on router hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SecondTab from '../views/SecondTab.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // Using hash mode. Not good for SEO
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
