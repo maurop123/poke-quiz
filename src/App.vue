d469d73718c09828db777f3e6562b837640d15e5
Mauro Perez <maurop123@gmail.com>
2023-04-04 04:55:19 -0400

    wip: add new pokeball logo

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import IntroSection from './components/IntroSection.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/pokeball-logo.png" width="125" height="125" />
 
     <div class="wrapper">
       <IntroSection msg="PokeMMO Training" />
--- src/components/IntroSection.vue
+++ src/components/IntroSection.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="white">{{ msg }}</h1>
     <h3>
       Type Quiz
     </h3>
